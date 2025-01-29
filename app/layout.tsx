088443e2904cdb595383b38c8ea74b2fc535beb3
vandodev <vandoaparecido@hotmail.com>
2025-01-29 08:21:50 -0300

    :link: :heavy_check_mark: :computer: :credit_card: Configurando fonte

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import { Red_Hat_Display } from 'next/font/google'
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"]
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -6,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body
-        className={`antialiased`}
+         className={`${redHatDisplay.className} bg-background-primary text-content-body antialiased`}
       >
         {children}
       </body>
