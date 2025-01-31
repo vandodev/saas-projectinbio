8ff3a1beea52618952db65171ab3a9db7e3a8d1b
vandodev <vandoaparecido@hotmail.com>
2025-01-31 08:37:55 -0300

    :link: :heavy_check_mark: :computer: :credit_card: Componente ProjectCard

--- app/components/commons/user-card.tsx
+++ app/components/commons/user-card.tsx
@@ -31,8 +31,9 @@ export default function UserCard() {
               <Icon />
             </button>
           ))}
-        </div>
-        <div className="flex flex-col gap-3 w-full h-[172px]">
+        </div>        
+      </div>
+      <div className="flex flex-col gap-3 w-full h-[172px]">
           <div className="w-full flex flex-col items-center gap-3">
             <Button className="w-full">Template SaaS - Compre Agora</Button>
             <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]">
@@ -40,7 +41,6 @@ export default function UserCard() {
             </button>
           </div>
         </div>
-      </div>
     </div>
   );
 }
