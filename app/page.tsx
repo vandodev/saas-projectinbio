4c7c2f1ffed3369edc5d19299adf31f0bd7231f9
vandodev <vandoaparecido@hotmail.com>
2025-02-01 08:11:53 -0300

    :link: :heavy_check_mark: :computer: :credit_card: Componente vídeo

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Hero from "./components/landing-page/hero";
 import Header from "./components/landing-page/header";
+import VideoExplanation from "./components/landing-page/video-explanation";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Hero />
       <Header />
-      
-      {/* 
       <VideoExplanation />
+      {/* 
+      
       <Pricing />
       <FAQ /> */}
     </div>
