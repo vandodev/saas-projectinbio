a4b3c4a446b0f515ed78676f777cf820ab9f92ae
vandodev <vandoaparecido@hotmail.com>
2025-02-03 09:42:24 -0300

    :link: :heavy_check_mark: :computer: :credit_card: Validando input com regex

--- app/(pages)/criar/create-link-form.tsx
+++ app/(pages)/criar/create-link-form.tsx
@@ -1,19 +1,26 @@
 "use client";
 
+import { useState } from "react";
+import { sanitizeLink } from "@/app/lib/utils";
+
 import Button from "@/app/components/ui/button";
 import TextInput from "@/app/components/ui/text-input";
-import { useState } from "react";
 
 export default function CreateLinkForm() {
 
   const [link, setLink] = useState("");
   const [error, setError] = useState("");
 
+  function handleLinkChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setLink(sanitizeLink(e.target.value));
+    setError("");
+  }
+
   return (
     <>
       <form  className="w-full flex items-center gap-2">
         <span>projectinbio.com/</span>
-        <TextInput value={link} />
+        <TextInput value={link} onChange={handleLinkChange}  />
         <Button className="w-[126px]">Criar</Button>
       </form>
       <div>
