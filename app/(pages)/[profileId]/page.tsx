716984ad1375c5be48e42ae1c7f2cfe95e9c8a67
vandodev <vandoaparecido@hotmail.com>
2025-02-04 08:40:53 -0300

    :link: :heavy_check_mark: :computer: :credit_card:  Condicional para dono do projeto

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -5,6 +5,7 @@ import { Plus } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { getProfileData } from "@/app/server/get-profile-data";
+import { auth } from "@/app/lib/auth";
 
 export default async function ProfilePage({
   params,
@@ -16,8 +17,14 @@ export default async function ProfilePage({
   const profileData = await getProfileData(profileId);
 
   if (!profileData) return notFound();
+  // console.log(profileData) 
 
-  console.log(profileData) 
+  const session = await auth();
+  const isOwner = profileData.userId === session?.user?.id;
+
+  // TODO: Adicionar page view
+  // Se o usuario não estiver mais no trial, nao deixar ver o projeto. Redirecionar para upgrade
+
 
   return (
     <div className="relative h-screen flex p-20 overflow-hidden">
@@ -40,10 +47,13 @@ export default async function ProfilePage({
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
-        <button className="w-[340px] h-[132px] rounded-[20px] bg-background-secondary flex items-center gap-2 justify-center hover:border hover:border-dashed border-border-secondary">
-          <Plus className="size-10 text-accent-green" />
-          <span>Novo projeto</span>
-        </button>
+        
+          {isOwner && (
+            <button className="w-[340px] h-[132px] rounded-[20px] bg-background-secondary flex items-center gap-2 justify-center hover:border hover:border-dashed border-border-secondary">
+              <Plus className="size-10 text-accent-green" />
+              <span>Novo projeto</span>
+            </button>
+          )}
       </div>
       <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
         <TotalVisits />
