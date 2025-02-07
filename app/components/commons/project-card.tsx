5fe288af1e23797317183e3dff17d0ee9f6f4508
vandodev <vandoaparecido@hotmail.com>
2025-02-07 07:29:30 -0300

    :link: :heavy_check_mark: :computer: :credit_card: Adicionando Links Personalizados

--- app/components/commons/project-card.tsx
+++ app/components/commons/project-card.tsx
@@ -2,6 +2,7 @@
 
 import { ProjectData } from "@/app/server/get-profile-data";
 import Link from "next/link";
+import { formatUrl } from "@/app/lib/utils";
 
 export default function ProjectCard({
   project,
@@ -12,10 +13,7 @@ export default function ProjectCard({
   isOwner: boolean;
   img: string;
 }) {
-  const projectUrl = project.projectUrl;
-  const formattedUrl = projectUrl.startsWith("http")
-    ? projectUrl
-    : `https://${projectUrl}`;
+  const formattedUrl = formatUrl(project?.projectUrl ?? "");
 
   function handleClick() {
     console.log("clicked"); // TODO: implementar analytics
@@ -41,9 +39,9 @@ export default function ProjectCard({
           )}
 
           <div className="flex flex-col">
-            <span className="text-white font-bold">{project.projectName}</span>
+            <span className="text-white font-bold">{project?.projectName}</span>
             <span className="text-content-body text-sm">
-              {project.projectDescription}
+              {project?.projectDescription}
             </span>
           </div>
         </div>
