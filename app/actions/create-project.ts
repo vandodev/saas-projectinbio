49e410da1ed54640e4929c5feeec945846ba3de9
vandodev <vandoaparecido@hotmail.com>
2025-02-07 09:40:24 -0300

    :link: :heavy_check_mark: :computer: :credit_card: Corrigindo/alterando local de salvar collection

--- app/actions/create-project.ts
+++ app/actions/create-project.ts
@@ -26,7 +26,7 @@ export async function createProject(formData: FormData) {
 
   try {
     await db
-      .collection("projects")
+      .collection("profiles")
       .doc(profileId)
       .collection("projects")
       .doc()
