74fd625f331750a3782d22606c33cd56dcc330cb
vandodev <vandoaparecido@hotmail.com>
2025-03-11 07:43:45 -0300

    :link: :heavy_check_mark: :computer: :credit_card: Corrigindo e validando webhook

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -4,89 +4,99 @@ import { db } from "@/app/lib/firebase";
  import Stripe from "stripe";
  
  export async function POST(req: NextRequest) {
-   try {
-     const body = await req.text();
-     const signature = req.headers.get("stripe-signature");
- 
-     const secret = process.env.STRIPE_WEBHOOK_SECRET;
- 
-     if (!signature || !secret) {
-       return new Error("Stripe webhook secret is not set");
-     }
- 
-     const event = stripe.webhooks.constructEvent(body, signature, secret);
- 
-        switch (event.type) {
-            case "checkout.session.completed":
-                // Usuario completou o checkout - assinatura ou pagamento unico
-                // console.log("usuário completou o checkout")
-                if (event.data.object.payment_status === "paid") {
-                    const userId = event.data.object.client_reference_id;
-                    if (userId) {
-                        await db.collection("users").doc(userId).update({
-                        isSubscribed: true,
-                        });
-                    }
-                }
+  try {
+    const body = await req.text();
+    const signature = req.headers.get("stripe-signature");
 
-                // Verificar se foi boleto
- 
-                if (
-                    event.data.object.payment_status === "unpaid" &&
-                    event.data.object.payment_intent
-                ) {
-                    const paymentIntent = await stripe.paymentIntents.retrieve(
-                    event.data.object.payment_intent.toString()
-                    );
-                    const hostedVoucherUrl =
-                    paymentIntent.next_action?.boleto_display_details
-                        ?.hosted_voucher_url;
-        
-                    if (hostedVoucherUrl) {
-                    const userEmail = event.data.object.customer_details?.email;
-                    console.log("Enviar email para o cliente com o boleto");
-                    }
-                } 
-            break;
+    const secret = process.env.STRIPE_WEBHOOK_SECRET;
 
-            case "checkout.session.async_payment_succeeded":
-                // Usuario pagou o boleto   
-                if (event.data.object.payment_status === "paid") {
-                    const userId = event.data.object.client_reference_id;
-                    if (userId) {
-                    await db.collection("users").doc(userId).update({
-                        isSubscribed: true,
-                    });
-                    }
-                }   
-            break;
-            case "customer.subscription.deleted":
-                // Usuario cancelou a assinatura  
-                
-                const subscription = event.data.object;
-                const customerId = subscription.customer as string;
-        
-                if (customerId) {
-                  const customer = (await stripe.customers.retrieve(
-                    customerId
-                  )) as Stripe.Customer;
-        
-                  if (customer && customer.metadata.userId) {
-                    const userId = customer.metadata.userId;
-        
-                    await db.collection("users").doc(userId).update({
-                      isSubscribed: false,
-                    });
-                  }
-                }
-                      
-            break;
-            
+    if (!signature || !secret) {
+      return new Error("Stripe webhook secret is not set");
+    }
+
+    const event = stripe.webhooks.constructEvent(body, signature, secret);
+
+    switch (event.type) {
+      case "checkout.session.completed":
+        // Usuario completou o checkout - assinatura ou pagamento unico
+        // console.log("usuário completou o checkout")
+        if (event.data.object.payment_status === "paid") {
+          const userId = event.data.object.client_reference_id;
+          if (userId) {
+            const userRef = db.collection('users').doc(userId);
+            const userDoc = await userRef.get();
+            if (userDoc.exists) {
+              await userRef.update({ isSubscribed: true });
+            } else {
+              await userRef.set({ isSubscribed: true });
+            }
+          }
         }
- 
-     return new NextResponse(null, { status: 200 });
-   } catch (error) {
-     console.error("Stripe webhook error", error);
-     return new NextResponse(null, { status: 500 });
-   }
+
+
+        // Verificar se foi boleto
+
+        if (
+            event.data.object.payment_status === "unpaid" &&
+            event.data.object.payment_intent
+        ) {
+            const paymentIntent = await stripe.paymentIntents.retrieve(
+            event.data.object.payment_intent.toString()
+            );
+            const hostedVoucherUrl =
+            paymentIntent.next_action?.boleto_display_details
+                ?.hosted_voucher_url;
+
+            if (hostedVoucherUrl) {
+            const userEmail = event.data.object.customer_details?.email;
+            console.log("Enviar email para o cliente com o boleto");
+            }
+        } 
+      break;
+
+      case "checkout.session.async_payment_succeeded":
+        // Usuario pagou o boleto   
+        if (event.data.object.payment_status === "paid") {
+          const userId = event.data.object.client_reference_id;
+          if (userId) {
+              const userRef = db.collection('users').doc(userId);
+              const userDoc = await userRef.get();
+              if (userDoc.exists) {
+                await userRef.update({ isSubscribed: true });
+              } else {
+                await userRef.set({ isSubscribed: true });
+            }
+          }
+        }   
+      break;
+      
+      case "customer.subscription.deleted":
+        // Usuario cancelou a assinatura  
+        
+        const subscription = event.data.object;
+        const customerId = subscription.customer as string;
+
+        if (customerId) {
+          const customer = (await stripe.customers.retrieve(
+            customerId
+          )) as Stripe.Customer;
+
+          if (customer && customer.metadata.userId) {
+            const userId = customer.metadata.userId;
+
+            await db.collection("users").doc(userId).update({
+              isSubscribed: false,
+            });
+          }
+        }
+                
+      break;
+        
+    }
+
+    return new NextResponse(null, { status: 200 });
+  } catch (error) {
+    console.error("Stripe webhook error", error);
+    return new NextResponse(null, { status: 500 });
+  }
 }
