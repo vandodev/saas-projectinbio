859f0c94279c5c9a3e1157ee0828f2deea35c817
vandodev <vandoaparecido@hotmail.com>
2025-03-11 08:20:50 -0300

    :link: :heavy_check_mark: :computer: :credit_card: Função botão minha página

--- app/components/landing-page/header.tsx
+++ app/components/landing-page/header.tsx
@@ -1,9 +1,13 @@
 import Button from "../ui/button";
 import { auth } from "@/app/lib/auth";
 import { manageAuth } from "@/app/actions/manage-auth";
+import Link from "next/link";
+ import { getProfileId } from "@/app/server/get-profile-data";
 
 export default async function Header() {
   const session = await auth();
+  const profileId = await getProfileId(session?.user?.id as string);
+  
   return (
     <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
       <div className="flex items-center gap-4">
@@ -11,7 +15,13 @@ export default async function Header() {
         <h3 className="text-white text-2xl font-bold">ProjectInBio</h3>
       </div>
       <div className="flex items-center gap-4">
-        {session && <Button>Minha Página</Button>}
+       
+        {session && (
+           <Link href={`/${profileId}`}>
+             <Button>Minha Página</Button>
+           </Link>
+        )}
+
         <form action={manageAuth}>
           <Button>{session ? "Sair" : "Login"}</Button>
         </form>
